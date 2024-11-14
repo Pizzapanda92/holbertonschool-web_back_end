c2c368e4e39a271ce2c69361c8fff3400e0d3304
Pizzapanda92 <fabio92@sfr.fr>
2024-11-14 19:40:47 +0100

    mplement a class named Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  Constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
+
+
