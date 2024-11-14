39850ad02926d92ad8ad330abddf770f9264a222
Pizzapanda92 <fabio92@sfr.fr>
2024-11-14 19:54:26 +0100

    supp evacuationWarningMessage

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -10,8 +10,4 @@ export default class Building {
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
-  }
 }
