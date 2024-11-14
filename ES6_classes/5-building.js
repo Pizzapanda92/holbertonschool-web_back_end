7a8c2c37fc2161905ec4bbfbcdb06e9e9a92f871
Pizzapanda92 <fabio92@sfr.fr>
2024-11-14 19:50:12 +0100

    coorection !== to ===

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
+    if (this.constructor === Building) {
       throw new Error("Building is an abstract class and cannot be instantiated directly");
     }
     this._sqft = sqft;
