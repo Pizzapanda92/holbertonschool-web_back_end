3499d2e2ef1c80791bd4109bc7dd780deadba27f
Pizzapanda92 <fabio92@sfr.fr>
2024-11-14 19:53:47 +0100

    change methode

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error("Building is an abstract class and cannot be instantiated directly");
-    }
     this._sqft = sqft;
+
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
   }
 
   get sqft() {
